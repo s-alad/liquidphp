97f257c2f657ebc2bc19c43d2d07b225f066d61b
s-alad <n7.saad@gmail.com>
2023-01-07 19:11:34 -0500

    proposal

--- proposal-alliance-art/index.php
+++ proposal-alliance-art/index.php
@@ -1,10 +1,10 @@
-<?php include("../headers.php"); 
+<?php include("../headers.php");
 
 ?>
- <main style="background-color: #272727;" class="pt-5">
+<main style="background-color: #272727;" class="pt-5">
 
-        <!-- breadcrumb section start -->
-       <!--<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+    <!-- breadcrumb section start -->
+    <!--<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
       <img class="d-block w-100" style="height: 300px;" src="assets/img/banner/page-banner.jpg">
@@ -29,38 +29,92 @@
             </div>
   </div>
 </div>-->
-        
-                <section class="about-wrapper-area section-padding fix">
+
+    <section class="about-wrapper-area section-padding fix">
 
 
-<!--
+        <!--
 
 
             <div class="container pt-1">
                 
-                  <?php //show_solution(); ?>
+                  <?php //show_solution(); 
+                    ?>
                 
             </div>
         </section>
 
 -->
 
-                <section class="funfacts-section pb-3 pt-5"><br />
+        <section class="funfacts-section pb-3 pt-5"><br />
             <div class="container">
                 <div class="row">
                     <div class="col-12">
                         <div class="">
                             <h2 class="h1 title-article text-white">Alliance Art & Community Youth Alvin Ailey Trip, October 10, 2006.</h2>
-							<!--<p class="text-white">By Laurence Shanet, Liquid Barrier Head Writer</p>-->
+                            <!--<p class="text-white">By Laurence Shanet, Liquid Barrier Head Writer</p>-->
                         </div>
                     </div>
                 </div>
-				<br />
+                <br />
                 <div class="row pt-40">
-					<div class="col-md-12">
-						<p class="text-white"></p>
-						<p class="text-white"></p>
-					</div>
+                    <div class="col-md-12">
+                        <p class="text-white">Dear Ms. Smith:</p>
+                        <p class="text-white">I know how much the performing and cultural arts means to you. Your commitment, hard work, and
+                            sacrifice ensure that the arts will continue to enrich the lives of children of all ages and adults too. In
+                            light of this affection and commitment, please consider our request for financial support to provide 20
+                            Irvington young people, ages 15 through 18, the opportunity to experience the magic of the Alvin
+                            Ailey American Dance Theater on December 9, 2006 at New York City Center.</p>
+                        <p class="text-white">An Afternoon with Alvin Ailey is the first collaboration between two community service organizations:
+                            Art 4 Kidz and Alliance Arts &amp; Community. As you might expect, the primary purpose of our program
+                            is to introduce the performing arts to a group of urban youngsters who might not otherwise have the
+                            opportunity. In fact, our An Afternoon with Alvin Ailey is the first of what
+                            we hope will be many cultural excursions to performing arts and cultural
+                            events throughout the tri-state area for Irvington youth of all ages. I am
+                            sure you can appreciate the excitement of attending a New York City
+                            theater performance and having dinner afterwards—especially during
+                            the holiday season. The cool night air coupled with brightly lit store
+                            windows are like icing on a cake after a memorable theater
+                            performance. In fact, I can remember going to see Alvin Ailey, Dance
+                            Theater of Harlem and Broadway performances such as Amadeus and The Phantom of the Opera as
+                            a youth with my family. Attending those performances years ago instilled in me an appreciation for
+                            the arts that remains today and I would like to pass on my appreciation to the children in my
+                            community.</p>
+                        <p class="text-white">In my family there were no cultural or economic barriers impeding my access to the arts; in fact my
+                            family encouraged it. However, many Irvington children come from single parent and no parent
+                            households that are both economically and culturally disadvantaged. Their caregivers, due to
+                            financial or other restrictions, cannot provide these children access to the arts—or worse their
+                            caregivers have little knowledge or appreciation for the arts themselves. Also, public school funding
+                            for the arts and arts education have all but evaporated. Consequently, opportunities for these
+                            children to embrace the arts on any level have all but disappeared. And adding insult to injury is the
+                            fact that New York City theater ticket prices have probably doubled over the last 10 years. Sadly, “a
+                            night out at the theater” is almost completely out of reach for most young people from my community
+                            and their families.</p>
+                        <p class="text-white">Our proposed An Afternoon with Alvin Ailey would, at least for one night, allow these youngsters to
+                            hurtle the social and economic barriers to the arts and deliver them from an environment that for
+                            some is filled with danger and hopelessness. On December 9 th , they will be thrust into a world of
+                            vibrant colors, agile movements, music, and boundless energy and creativity of modern dance.</p>
+                        <p class="text-white">The experience of seeing Alvin Ailey’s remarkable dancers performing at such a high skill level will in
+                            turn build their self confidence to excel at any endeavor of their choosing including the performing
+                            arts. Seeing Alvin Ailey no doubt will be an experience these youngsters will remember for a lifetime
+                            and plant in them an appreciation for the arts that they can pass along to their siblings, friends, others
+                            in the community and, one day, their children. I feel honored to be part of this experience. Perhaps
+                            you will too!</p>
+                        <p class="text-white">Please feel free to contact me with any questions you have about our proposal request at 973-373-
+                            8595. Thank you for your time and consideration.</p>
+                        <p class="text-white">Sincerely,</p>
+                        <p class="text-white">Frank Bryant, III <br>
+                            Alliance Art &amp; Community</p>
+                        <p class="text-white"></p>
+                        <p class="text-white"></p>
+                        <p class="text-white"></p>
+                        <p class="text-white"></p>
+                        <p class="text-white"></p>
+                        <p class="text-white"></p>
+                        <p class="text-white"></p>
+                        <p class="text-white"></p>
+
+                    </div>
                     <!--<div class="col-md-6 col-sm-6 border-right border-danger">
                         <div>
                             <p class="text-white">Liquid Barrier firmly supports workforce diversity and inclusion and social and cultural respect for the following groups: persons of indigenous heritage; members of the African diaspora; persons of Latino heritage; persons with disabilities; persons identifying as LGBTQ; and persons of disparate religious backgrounds.</p>
@@ -73,14 +127,14 @@
                     </div>-->
                 </div>
             </div>
-       </section>
-        </main>
-        <div class="scroll-top not-visible">
-        <i class="fa fa-angle-up"></i>
-    </div>
-    <!-- Scroll to Top End -->
+        </section>
+</main>
+<div class="scroll-top not-visible">
+    <i class="fa fa-angle-up"></i>
+</div>
+<!-- Scroll to Top End -->
 
 
 
-    <!-- footer section start -->
-    <?php include("../footer.php") ?>
+<!-- footer section start -->
+<?php include("../footer.php") ?>
