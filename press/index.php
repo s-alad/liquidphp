6a0d2aad16be73160e6954344e681d94de0acf86
s-alad <n7.saad@gmail.com>
2023-01-15 19:49:18 -0500

    fixes

--- press/index.php
+++ press/index.php
@@ -1,15 +1,25 @@
-<?php include '../headers.php';?>
+<?php include '../headers.php'; ?>
 
-    <main>
+<main class="pt-5">
 
-        <!-- breadcrumb section start -->
-        <div class="breadcrumb-area bg-img" style="padding-top: 80px;" data-bg="<?php echo $baseuri ?>/assets/img/banner/page-banner.jpg">
+    <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img class="d-block w-100" style="height: 323px;" src="<?php echo $baseuri ?>/assets/img/banner/page-banner.jpg">
+            </div>
+            <!--<div class="carousel-item">
+      <img class="d-block w-100" style="height: 323px;" src="assets/img/banner/page-banner.jpg">
+
+    </div>-->
+            <!--<div class="carousel-item">
+      <img class="d-block w-100" src="assets/img/banner/page-banner.jpg">
+    </div>-->
             <div class="container">
                 <div class="row">
                     <div class="col-12">
                         <div class="breadcrumb-wrap">
                             <nav aria-label="breadcrumb">
-                                <h2 class="breadcrumb-title text-left">NEWS</h2>
+                                <h2 class="breadcrumb-title text-left">Our Team</h2>
 
                             </nav>
                         </div>
@@ -17,24 +27,55 @@
                 </div>
             </div>
         </div>
-        <!-- breadcrumb section end -->
+    </div>
 
-        <!-- service section start -->
-        <section class="service-details-wrapper section-padding pb-125">
-            <div class="container">
-                <div class="row">
+    <!-- breadcrumb section start -->
+    <!-- <div class="breadcrumb-area bg-img" style="padding-top: 80px;" data-bg="<?php echo $baseuri ?>/assets/img/banner/page-banner.jpg">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="breadcrumb-wrap">
+                        <nav aria-label="breadcrumb">
+                            <h2 class="breadcrumb-title text-left">NEWS</h2>
 
-                    <div class="col-lg-8 pl-lg-45 order-1 order-lg-2">
-                        <div class="service-details">
-
-                            <h2 class="h1 pt-5 pb-3">News</h2>
-                            <p class="text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione, sunt perspiciatis error id ipsa atque unde quis dolore nobis eum aperiam enim blanditiis pariatur inventore eius commodi consectetur ut. Totam, assumenda! Laboriosam possimus, corporis dicta!</p>
-                            <p class="text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores aliquid quod, officiis unde nostrum itaque! Adipisci dolorum, ab dolor, exercitationem praesentium dolorem quo voluptatum itaque dignissimos, sit esse cupiditate. Doloremque rerum similique a nobis placeat in illum, quo quaerat, ut repellat, fuga itaque? Nihil mollitia nisi, nam, accusantium nemo consequuntur reiciendis autem dicta consequatur earum beatae dolor distinctio, debitis repudiandae?</p>
-
-
-                        </div>
+                        </nav>
                     </div>
-                    <!-- <div class="col-lg-8 pl-lg-45 order-1 order-lg-2">
+                </div>
+            </div>
+        </div>
+    </div> -->
+    <!-- breadcrumb section end -->
+
+    <!-- service section start -->
+    <section class="service-details-wrapper">
+        <div class="container mt-5">
+            <div class="content">
+                <div class="row" style="max-width: 800px !important;">
+                    <h1>News</h1><br />
+                    <br />
+
+                    <p class="text-white">
+                        <!--<img src="<?php //echo $baseuri 
+                                        ?>/img/bodies.JPG" alt="bodies" /><br />-->
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione, sunt perspiciatis error id ipsa atque unde quis dolore nobis eum aperiam enim blanditiis pariatur inventore eius commodi consectetur ut. Totam, assumenda! Laboriosam possimus, corporis dicta!
+                    </p>
+                    <p class="text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores aliquid quod, officiis unde nostrum itaque! Adipisci dolorum, ab dolor, exercitationem praesentium dolorem quo voluptatum itaque dignissimos, sit esse cupiditate. Doloremque rerum similique a nobis placeat in illum, quo quaerat, ut repellat, fuga itaque? Nihil mollitia nisi, nam, accusantium nemo consequuntur reiciendis autem dicta consequatur earum beatae dolor distinctio, debitis repudiandae?</p>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <!-- <div class="row">
+                <div class="col-lg-8 pl-lg-45 order-1 order-lg-2">
+                    <div class="service-details">
+
+                        <h1>News</h1>
+                        <p class="text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione, sunt perspiciatis error id ipsa atque unde quis dolore nobis eum aperiam enim blanditiis pariatur inventore eius commodi consectetur ut. Totam, assumenda! Laboriosam possimus, corporis dicta!</p>
+                        <p class="text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores aliquid quod, officiis unde nostrum itaque! Adipisci dolorum, ab dolor, exercitationem praesentium dolorem quo voluptatum itaque dignissimos, sit esse cupiditate. Doloremque rerum similique a nobis placeat in illum, quo quaerat, ut repellat, fuga itaque? Nihil mollitia nisi, nam, accusantium nemo consequuntur reiciendis autem dicta consequatur earum beatae dolor distinctio, debitis repudiandae?</p>
+
+
+                    </div>
+                </div> -->
+            <!-- <div class="col-lg-8 pl-lg-45 order-1 order-lg-2">
                         <div class="service-details">
 
                             <h2 class="h1 title pt-5 pb-3">Media</h2>
@@ -44,7 +85,7 @@
 
                         </div>
                     </div> -->
-					<!--
+            <!--
                     <div class="col-lg-8 pl-lg-45 order-1 order-lg-2">
                         <div class="service-details">
 
@@ -67,18 +108,18 @@
                     </div>
 					-->
 
-                </div>
-            </div>
-        </section>
-        <!-- service section end -->
-    </main>
+        </div>
+        </div>
+    </section>
+    <!-- service section end -->
+</main>
 
-    <!-- Scroll to top start -->
-    <div class="scroll-top not-visible bg-danger">
-    	<i class="fa fa-angle-up "></i>
-    </div>
-    <!-- Scroll to Top End -->
+<!-- Scroll to top start -->
+<div class="scroll-top not-visible bg-danger">
+    <i class="fa fa-angle-up "></i>
+</div>
+<!-- Scroll to Top End -->
 
-    <!-- footer section start -->
+<!-- footer section start -->
 
 <?php include '../footer.php'; ?>
